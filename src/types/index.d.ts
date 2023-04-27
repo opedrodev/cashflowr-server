819a371d06a9572f03e42edbcd5a818c09b4e687
Pedro Silva <pvasilva.dev@hotmail.com>
2023-04-26 22:06:37 -0300

    feat: create types

---
+++ src/types/index.d.ts
@@ -0,0 +1,20 @@
+export type TExpense = {
+    description: string;
+    category: string;
+    value: number;
+    date: Date;
+};
+
+export type TWallet = {
+    balance: number;
+    income: number;
+    outcome: number;
+    expenses: TExpense[];
+};
+
+export type TUser = {
+    name: string;
+    email: string;
+    password: string;
+    wallet: TWallet;
+};
