cc40ee83be49eacebffdf3af02b178df1fbfa6ed
Pedro Silva <pvasilva.dev@hotmail.com>
2023-04-27 01:01:26 -0300

    feat: add helper class CustomError

---
+++ src/helpers/CustomError.ts
@@ -0,0 +1,12 @@
+class CustomError extends Error {
+    public readonly message: string;
+    public readonly status: number;
+
+    constructor(message: string, status: number) {
+        super(message);
+        this.message = message;
+        this.status = status;
+    }
+}
+
+export default CustomError;
