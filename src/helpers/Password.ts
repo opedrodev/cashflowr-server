202273750ddc0a7fac008c4ef4674f6e3e6db3b3
Pedro Silva <pvasilva.dev@hotmail.com>
2023-04-27 01:15:57 -0300

    feat: add helper class Password

---
+++ src/helpers/Password.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcryptjs';
+
+class Password {
+
+    public static hash(password: string): string {
+        return bcrypt.hashSync(password, 10);
+    }
+
+    public static compare(password: string, hashedPassword: string): boolean {
+        return bcrypt.compareSync(password, hashedPassword);
+    }
+
+}
+
+export default Password;
