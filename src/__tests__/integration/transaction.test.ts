d03878ecf3208ae4bdeab4cadfe4f046aa2da741
Pedro Silva <pvasilva.dev@hotmail.com>
2023-04-30 03:22:20 -0300

    test: implement tests for endpoint transactions

---
+++ src/__tests__/integration/transaction.test.ts
@@ -0,0 +1,97 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+
+import { Model } from 'mongoose';
+import App from '../../App';
+import TransactionMocks from '../mocks/transaction.mock';
+
+const { app } = new App();
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Transaction', () => {
+    describe('GET /transactions/:id', () => {
+        afterEach(() => sinon.restore());
+
+        it('should return all transactions from a user', async () => {
+            sinon.stub(Model, 'findById').resolves(TransactionMocks.USER);
+
+            const res = await chai
+                .request(app)
+                .get('/transactions/123');
+
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body).to.have.lengthOf(1);
+            expect(res.body[0]).to.be.deep.equal(TransactionMocks.TRANSACTION);
+        });
+
+        it('should throw an error when user not found', async () => {
+            sinon.stub(Model, 'findById').resolves(null);
+
+            const res = await chai
+                .request(app)
+                .get('/transactions/123');
+
+            expect(res.status).to.equal(404);
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.be.equal('User not found');
+        });
+    });
+
+    describe('POST /transactions/:id', () => {
+        afterEach(() => sinon.restore());
+
+        it('should create a new income transaction', async () => {
+            sinon.stub(Model, 'findByIdAndUpdate').resolves(TransactionMocks.USER);
+
+            const res = await chai
+                .request(app)
+                .post('/transactions/123')
+                .send(TransactionMocks.TRANSACTION_INCOME);
+
+            expect(res.status).to.equal(201);
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.be.equal('Transaction created successfully');
+        });
+
+        it('should create a new outcome transaction', async () => {
+            sinon.stub(Model, 'findByIdAndUpdate').resolves(TransactionMocks.USER);
+
+            const res = await chai
+                .request(app)
+                .post('/transactions/123')
+                .send(TransactionMocks.TRANSACTION_OUTCOME);
+
+            expect(res.status).to.equal(201);
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.be.equal('Transaction created successfully');
+        });
+
+        it('should throw an error when user not found', async () => {
+            sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+
+            const res = await chai
+                .request(app)
+                .post('/transactions/123')
+                .send(TransactionMocks.TRANSACTION_OUTCOME);
+
+            expect(res.status).to.equal(404);
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.be.equal('User not found');
+        });
+
+        it('should throw an if called with invalid data', async () => {
+            sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+
+            const res = await chai
+                .request(app)
+                .post('/transactions/123')
+                .send(TransactionMocks.INVALID_TRANSACTION);
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('message');
+        });
+    });
+});
---
+++ src/__tests__/mocks/transaction.mock.ts
@@ -0,0 +1,57 @@
+const USER = {
+    wallet: {
+        balance: 1000,
+        income: 0,
+        outcome: 0,
+        transactions: [
+            {
+                id: '123',
+                type: 'income',
+                description: 'Test Transaction',
+                category: 'Test',
+                value: 1000,
+                date: new Date(),
+            },
+        ],
+    },
+};
+
+const TRANSACTION = {
+    id: '123',
+    type: 'income',
+    description: 'Test Transaction',
+    category: 'Test',
+    value: 1000,
+    date: new Date().toISOString(),
+};
+
+const TRANSACTION_INCOME = {
+    type: 'income',
+    description: 'Salary',
+    category: 'Work',
+    value: 1200,
+};
+
+const TRANSACTION_OUTCOME = {
+    type: 'outcome',
+    description: 'Salary',
+    category: 'Work',
+    value: 1200,
+};
+
+const INVALID_TRANSACTION = {
+    type: 'invalid',
+    description: 'Salary',
+    category: 'Work',
+    value: 1200,
+};
+
+const TransactionMocks = {
+    USER,
+    TRANSACTION,
+    TRANSACTION_INCOME,
+    TRANSACTION_OUTCOME,
+    INVALID_TRANSACTION,
+};
+
+export default TransactionMocks;
